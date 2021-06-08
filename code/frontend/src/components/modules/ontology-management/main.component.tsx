a1c56ac46572cda657545c5fb52188792fcba262
massih10 <sayed@datalake6>
2021-06-08 10:48:01 +0000

    added Fuseki functionality

--- code/frontend/src/components/modules/ontology-management/main.component.tsx
+++ code/frontend/src/components/modules/ontology-management/main.component.tsx
@@ -17,69 +17,152 @@ import { ContainerGrid } from "./styles";
 import FileInput from "../../common/FileInput";
 import Item from "../../common/item";
 
+/* Sayeds Part */
+import { useState } from "react";
+import Query from "./query.tsx";
+import Search from "./search.tsx";
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+/* Sayeds Part End */
+
 const Main: React.FC<IViewProps<ViewModel>> = observer(({ viewModel }) => {
   const { t } = useTranslation();
 
+  /* Sayeds Part Start */
+  const [queryinfos, setQueryinfos] = useState([]);
+
+  const addQueryHandler = (queryinfo) => {
+    setQueryinfos([...queryinfos, queryinfo]);
+  };
+
+  const [searchinfos, setSearchinfos] = useState([]);
+
+  const addSearchHandler = (searchinfo) => {
+    setSearchinfos([...searchinfos, searchinfo]);
+  };
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+  /* Sayeds Part End*/
+
   return (
     <React.Fragment>
-      <TopRightFab
-        variant="extended"
-        color="primary"
-        onClick={() => viewModel.openUploadDialog()}
-      >
-        <AddIcon />
-        {t("generic.add_new")}
-      </TopRightFab>
-      <Typography variant="h6">{t("ontology_management.title")}</Typography>
-      <Typography variant="subtitle1">
-        {t("ontology_management.description")}
-      </Typography>
-      <ContainerGrid container spacing={0}>
-        {viewModel.ontologies.map((item) => (
-          <Item
-            key={item.id}
-            title={item.name}
-            onDelete={() => viewModel.delete(item)}
-          />
-        ))}
-      </ContainerGrid>
-      <Dialog
-        open={viewModel.isUploadDialogOpen}
-        disableAutoFocus
-        onClose={() => viewModel.closeUploadDialog()}
-      >
-        <DialogTitle>{t("ontology_management.upload.title")}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {t("ontology_management.upload.description")}
-          </DialogContentText>
-          <TextField
-            autoFocus
-            onChange={(e) => viewModel.setUploadName(e.target.value)}
-            value={viewModel.uploadName}
-            margin="dense"
-            label={t("generic.name")}
-            fullWidth
-          />
-          <FileInput
-            label={t("generic.file")}
-            accept=".owl"
-            onChange={(v) => viewModel.setUploadFile(v)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={() => viewModel.closeUploadDialog()}>
-            {t("generic.cancel")}
-          </Button>
-          <Button
-            color="primary"
-            disabled={!viewModel.canUpload}
-            onClick={() => viewModel.upload()}
-          >
-            {t("generic.upload")}
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+        <Tab label=" Upload an Ontology-File " {...a11yProps(0)} />
+        <Tab label=" Query an Ontology " {...a11yProps(1)} />
+        <Tab label=" Search for Keyword " {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <TopRightFab
+          variant="extended"
+          color="primary"
+          onClick={() => viewModel.openUploadDialog()}
+        >
+          <AddIcon />
+          {t("generic.add_new")}
+        </TopRightFab>
+        <Typography variant="h6">{t("ontology_management.title")}</Typography>
+        <Typography variant="subtitle1">
+          {t("ontology_management.description")}
+        </Typography>
+        <ContainerGrid container spacing={0}>
+          {viewModel.ontologies.map((item) => (
+            <Item
+              key={item.id}
+              title={item.name}
+              onDelete={() => viewModel.delete(item)}
+            />
+          ))}
+        </ContainerGrid>
+        <Dialog
+          open={viewModel.isUploadDialogOpen}
+          disableAutoFocus
+          onClose={() => viewModel.closeUploadDialog()}
+        >
+          <DialogTitle>{t("ontology_management.upload.title")}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {t("ontology_management.upload.description")}
+            </DialogContentText>
+            <TextField
+              autoFocus
+              onChange={(e) => viewModel.setUploadName(e.target.value)}
+              value={viewModel.uploadName}
+              margin="dense"
+              label={t("generic.name")}
+              fullWidth
+            />
+            <FileInput
+              label={t("generic.file")}
+              accept=".owl"
+              onChange={(v) => viewModel.setUploadFile(v)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={() => viewModel.closeUploadDialog()}>
+              {t("generic.cancel")}
+            </Button>
+            <Button
+              color="primary"
+              disabled={!viewModel.canUpload}
+              onClick={() => viewModel.upload()}
+            >
+              {t("generic.upload")}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Query addQueryHandler={addQueryHandler} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Search addSearchHandler={addSearchHandler} />
+      </TabPanel>
     </React.Fragment>
   );
 });
