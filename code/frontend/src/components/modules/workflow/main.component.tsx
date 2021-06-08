2393dccd9300db385d75564f86a247561fedf1ed
Maher Fallouh <maher.fallouh@gmail.com>
2021-06-08 11:42:59 +0200

    + initial workflow

--- code/frontend/src/components/modules/workflow/main.component.tsx
+++ code/frontend/src/components/modules/workflow/main.component.tsx
@@ -1,30 +1,88 @@
-import React from 'react'
-import { observer } from 'mobx-react-lite'
-import ViewModel from './viewModel'
-import IViewProps from '../../../models/iViewProps'
-import { useTranslation } from 'react-i18next'
-import workspacesStore from '../../../stores/workspaces.store'
-import Sidebar from './sidebar'
-import { useLocalObservable } from 'mobx-react-lite'
+import React, { useRef, useState } from "react";
+import { observer } from "mobx-react-lite";
+import ViewModel from "./viewModel";
+import IViewProps from "../../../models/iViewProps";
+import { useTranslation } from "react-i18next";
+import workspacesStore from "../../../stores/workspaces.store";
+import Sidebar from "./sidebar";
+import { useLocalObservable } from "mobx-react-lite";
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  removeElements,
+  Controls,
+  OnLoadParams,
+  Connection,
+  Edge,
+  FlowElement,
+  Node as FlowNode,
+  Elements,
+  XYPosition,
+} from "react-flow-renderer";
+import { v4 as uuidv4 } from "uuid";
 
-
-const elements = [
-    { id: '1', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
-    // you can also pass a React component as a label
-    { id: '2', data: { label: <div>Node 2</div> }, position: { x: 100, y: 100 } },
-    { id: 'e1-2', source: '1', target: '2', animated: true },
-];
 const Main: React.FC<IViewProps<ViewModel>> = observer(({ viewModel }) => {
-    const { t } = useTranslation()
-    return null
-    return (
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-            <div style={{ flex: 1 }}>
+  const reactFlowWrapper = useRef<HTMLDivElement | null>(null);
+  const [reactFlowInstance, setReactFlowInstance] = useState<any>(null);
+  const [elements, setElements] = useState<Elements<any>>([]);
 
-            </div>
-            <Sidebar viewModel={viewModel} />
+  const onConnect = (params: Edge<any> | Connection) =>
+    setElements((els) => addEdge(params, els));
+
+  const onElementsRemove = (elementsToRemove: Elements<any>) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+
+  const onLoad = (_reactFlowInstance: OnLoadParams<any>) =>
+    setReactFlowInstance(_reactFlowInstance);
+
+  const generateNode = (type: string, position: XYPosition) => {
+    let node: FlowNode<any> = {
+      id: uuidv4(),
+      position,
+      data: { label: `${type} node` },
+    };
+
+    if (indexOf)
+    return node;
+  };
+
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  };
+
+  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    if (!reactFlowWrapper.current) return;
+    const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+    const type = event.dataTransfer.getData("application/reactflow");
+    const position = reactFlowInstance.project({
+      x: event.clientX - reactFlowBounds.left,
+      y: event.clientY - reactFlowBounds.top,
+    });
+
+    setElements((es) => es.concat(generateNode(type, position)));
+  };
+
+  return (
+    <div style={{ flex: 1, display: "flex" }}>
+      <ReactFlowProvider>
+        <Sidebar viewModel={viewModel} />
+        <div style={{ flex: 1 }} ref={reactFlowWrapper}>
+          <ReactFlow
+            elements={elements}
+            onConnect={onConnect}
+            onElementsRemove={onElementsRemove}
+            onLoad={onLoad}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+          >
+            <Controls />
+          </ReactFlow>
         </div>
-    )
-})
+      </ReactFlowProvider>
+    </div>
+  );
+});
 
-export default Main
+export default Main;
--- code/frontend/src/components/modules/workflow/sidebar/main.component.tsx
+++ code/frontend/src/components/modules/workflow/sidebar/main.component.tsx
@@ -1,80 +1,51 @@
-import React from 'react'
-import { observer } from 'mobx-react-lite'
-import ViewModel from '..//viewModel'
-import IViewProps from '../../../../models/iViewProps'
-import { useTranslation } from 'react-i18next'
-import { useLocalObservable } from 'mobx-react'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import Divider from '@material-ui/core/Divider'
-import ListItemText from '@material-ui/core/ListItemText'
-import { makeStyles } from '@material-ui/core/styles'
-import { Theme } from '@material-ui/core/styles'
-import { createStyles } from '@material-ui/core/styles'
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: 'flex',
-        },
-        appBar: {
-            zIndex: theme.zIndex.drawer + 1,
-        },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-        drawerPaper: {
-            width: drawerWidth,
-        },
-        drawerContainer: {
-            overflow: 'auto',
-        },
-        content: {
-            flexGrow: 1,
-            padding: theme.spacing(3),
-        },
-    }),
-);
+import React from "react";
+import { observer } from "mobx-react-lite";
+import ViewModel from "..//viewModel";
+import IViewProps from "../../../../models/iViewProps";
+import { useTranslation } from "react-i18next";
+import { useLocalObservable } from "mobx-react";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core/styles";
+import { Theme } from "@material-ui/core/styles";
+import { createStyles } from "@material-ui/core/styles";
+import { Container } from "./styles";
 
 const Main: React.FC<IViewProps<ViewModel>> = observer(({ viewModel }) => {
-    const { t } = useTranslation()
-    const classes = useStyles();
+  const { t } = useTranslation();
 
-    return (
-        <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            anchor='right'
-            classes={{
-                paper: classes.drawerPaper,
-            }}
+  const onDragStart = (
+    event: React.DragEvent<HTMLDivElement>,
+    nodeType: string
+  ) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <Container>
+      <List>
+        <ListItem
+          button
+          onDragStart={(event) => onDragStart(event, "data_source")}
+          draggable
         >
-            <div className={classes.drawerContainer}>
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </div>
-        </Drawer>
-    )
-})
+          <ListItemText primary={t("workflow.items.data_source")} />
+        </ListItem>
+        <ListItem
+          button
+          onDragStart={(event) => onDragStart(event, "join")}
+          draggable
+        >
+          <ListItemText primary={t("workflow.items.join")} />
+        </ListItem>
+      </List>
+    </Container>
+  );
+});
 
-export default Main
-
-
+export default Main;
