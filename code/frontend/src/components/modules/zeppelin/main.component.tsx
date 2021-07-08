2ddc8915cb7a85da0c3c1794556aa0c4c7245fc6
Muhammad Noman <muhammad.noman@rwth-aachen.de>
2021-07-08 16:41:17 +0200

    zeppelin iframe fix

--- code/frontend/src/components/modules/zeppelin/main.component.tsx
+++ code/frontend/src/components/modules/zeppelin/main.component.tsx
@@ -8,11 +8,11 @@ import Iframe from 'react-iframe'
 const Main: React.FC<IViewProps<ViewModel>> = observer(({ viewModel }) => {
   const { t } = useTranslation();
   return (
-    <div style = {{height:"100vh"}} width="1500">
+    <div style = {{height:"100%"}}>
         <iframe
           src="http://localhost:1234/#/"
-          style = {{height:"100vh"}}
-          width="1500"
+          style = {{height:"100%"}}
+          width="100%"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
