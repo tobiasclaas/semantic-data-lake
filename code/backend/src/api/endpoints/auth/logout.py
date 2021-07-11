b9e460c803165d3fd0dc715ba97a16a138f93d5a
zaid <abdullah.zaid@rwth-aachen.de>
2021-07-11 21:36:12 +0200

    Backend API Authorization Documentation

--- code/backend/src/api/endpoints/auth/logout.py
+++ code/backend/src/api/endpoints/auth/logout.py
@@ -5,13 +5,26 @@ from werkzeug.exceptions import InternalServerError
 
 
 class Logout(Resource):
+
     
     def post(self):
+        """
+        # ==================================================================================================
+        Main return function in logout page.
+        :return:
+        # ==================================================================================================
+        """
         try:
+            # ==================================================================================================
+            #Successfully logout
+            # ==================================================================================================
             response = jsonify({
                 "logged out": True
             })
             unset_jwt_cookies(response)
             return response
         except Exception as err:
+            # ==================================================================================================
+            #error while logging out
+            # ==================================================================================================
             raise InternalServerError(f"Exception while logging out: \n\t {err}")
