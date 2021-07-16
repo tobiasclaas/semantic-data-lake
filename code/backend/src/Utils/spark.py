46253f7a4bac87d28bf6fd0f1803fd23f06913e8
Muhammad Noman <muhammad.noman@rwth-aachen.de>
2021-07-16 12:27:20 +0200

    SparkHelper code revert

--- code/backend/src/Utils/spark.py
+++ code/backend/src/Utils/spark.py
@@ -15,16 +15,12 @@ class SparkHelper:
         self.settings = Settings()
         try:
             conf = SparkConf().set("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0," +
-                                                          "org.postgresql:postgresql:42.2.18")
+                                                          "org.postgresql:postgresql:42.2.18," +
+                                                          "com.databricks:spark-xml_2.12:0.10.0")
             SparkContext(master="local", conf=conf) # For fixing "Failed to create pysparkSession" error        
             self.spark_session = SparkSession.builder \
                 .master(self.settings.spark_master) \
                 .appName(app_name) \
-                .config('spark.jars.packages',
-                        "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0," +
-                        "org.postgresql:postgresql:42.2.18," +
-                        "com.databricks:spark-xml_2.12:0.10.0"
-                        ) \
                 .getOrCreate() \
                 
         except Exception as err:
