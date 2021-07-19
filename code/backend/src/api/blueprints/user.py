f8bf20d60342f635d1f777be86db3b6afa734ef2
Tobias Claas <tobias.claas@rwth-aachen.de>
2021-07-19 20:21:47 +0200

    - fixed error

--- code/backend/src/api/blueprints/user.py
+++ code/backend/src/api/blueprints/user.py
@@ -1,13 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from api.endpoints.users import Users, Current
+from api.endpoints.users import Users
 
 USER_BLUEPRINT = Blueprint("user", __name__)
 
 users_routes = ["/users", "/users/<email>"]
 Api(USER_BLUEPRINT).add_resource(Users, *users_routes)
-
-current_routes = ["/users/current"]
-Api(USER_BLUEPRINT).add_resource(Current, *current_routes)
-
