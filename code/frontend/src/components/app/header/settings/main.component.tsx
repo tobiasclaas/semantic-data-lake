277dcf6e3cd52c1c3304c3b29d8fb8aaab77b964
Maher Fallouh <maher.fallouh@gmail.com>
2021-07-19 20:38:39 +0200

    + hide settings button when not logged in

--- code/frontend/src/components/app/header/settings/main.component.tsx
+++ code/frontend/src/components/app/header/settings/main.component.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import SettingsIcon from '@material-ui/icons/Settings';
-import settingsDialogStore from '../../../../stores/settings.store';
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import SettingsIcon from "@material-ui/icons/Settings";
+import settingsDialogStore from "../../../../stores/settings.store";
+import appStore from "../../../../stores/app.store";
 
 const SettingsComponent: React.FC = () => {
-    return (
-        <React.Fragment>
-            <IconButton
-                color="inherit"
-                onClick={() => settingsDialogStore.open()}
-                edge='end'
-            >
-                <SettingsIcon />
-            </IconButton>
-        </React.Fragment>
-    );
-}
+  if (!appStore.isLoggedIn) return null;
 
-export default SettingsComponent
+  return (
+    <React.Fragment>
+      <IconButton
+        color="inherit"
+        onClick={() => settingsDialogStore.open()}
+        edge="end"
+      >
+        <SettingsIcon />
+      </IconButton>
+    </React.Fragment>
+  );
+};
+
+export default SettingsComponent;
