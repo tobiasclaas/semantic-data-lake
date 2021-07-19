42ce78c23bffb6097b79f90f1a1539d502812184
Maher Fallouh <maher.fallouh@gmail.com>
2021-07-19 20:52:03 +0200

    + logout button

--- code/frontend/src/components/app/header/settings/main.component.tsx
+++ code/frontend/src/components/app/header/settings/main.component.tsx
@@ -3,12 +3,21 @@ import IconButton from "@material-ui/core/IconButton";
 import SettingsIcon from "@material-ui/icons/Settings";
 import settingsDialogStore from "../../../../stores/settings.store";
 import appStore from "../../../../stores/app.store";
+import routingStore from "../../../../stores/routing.store";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { observer } from "mobx-react";
 
-const SettingsComponent: React.FC = () => {
+const SettingsComponent: React.FC = observer(() => {
   if (!appStore.isLoggedIn) return null;
 
   return (
     <React.Fragment>
+      <IconButton
+        color="inherit"
+        onClick={() => routingStore.history.push("/login")}
+      >
+        <ExitToAppIcon />
+      </IconButton>
       <IconButton
         color="inherit"
         onClick={() => settingsDialogStore.open()}
@@ -18,6 +27,6 @@ const SettingsComponent: React.FC = () => {
       </IconButton>
     </React.Fragment>
   );
-};
+});
 
 export default SettingsComponent;
